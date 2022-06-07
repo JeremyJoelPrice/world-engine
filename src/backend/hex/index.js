191d572e312ed20df79038c44e33d195b701fd75
Jeremy Joel Price <JeremyJoelPrice@gmail.com>
2022-06-07 20:35:59 +0100

    implement basic hex point of interest rolling

--- src/backend/hex/index.js
+++ src/backend/hex/index.js
@@ -1,3 +1,17 @@
+const { recursivelyGetRandomElement } = require("../table roller/tableUtils");
+const { customTables } = require("../table roller/tables");
+
+export const rollForPointOfInterest = (numOfRolls) => {
+	const results = [];
+
+	for (let i = 0; i < numOfRolls; i++) {
+		const roll = Math.floor(Math.random() * 4 + 1);
+
+		results.push(roll === 1 ? getPointOfInterest() : "empty hex");
+	}
+	return results;
+};
+
 function getPointOfInterest() {
-	
+	return recursivelyGetRandomElement(customTables.hexPointsOfInterest);
 }
