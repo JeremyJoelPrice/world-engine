6f7d784b961531bfc10d09356c173525f978f510
Jeremy Joel Price <JeremyJoelPrice@gmail.com>
2022-06-26 11:59:02 +0100

    create location tags index file & add rural villages tables

---
+++ src/backend/table roller/tables/Location Tags/WWN/ruralVillages.js
@@ -0,0 +1,84 @@
+exports.rationaleForTheCommunitiesExistance = [
+	"Once a garrison aulpost of a nation",
+	"A mine or quarry, perhaps now exhausted",
+	"A spot where refuges of a calamity settled",
+	"Holy ground or a lample to a particular faith",
+	"A plant or animal grows very well here",
+	"It's a safe waypost on a trade roule",
+	"Refuge for a despised minority or group",
+	"A bandit camp that went legitimate",
+	"A self-ruled home for demihuman residents",
+	"It's a safe base for salvage or ruin plundering",
+	"Decayed remnant of an ancient city",
+	"It grew up around a lordly manar or estate"
+];
+exports.whoRunsIt = [
+	"A heraditary headman",
+	"A reave pickad by a lord",
+	"A temple representative",
+	"A council of elders",
+	"A crual and feared bully",
+	"A popularly-chosan chief",
+	"A dreaded sorcerer",
+	"A pragmatic warlord",
+	"The richest native there",
+	"A traditional squire",
+	"A patriarch or matriarch",
+	"An organizations envoy"
+];
+exports.significantLocals = [
+	"The village harlot",
+	"The worst local bully",
+	"A gifted young peasant",
+	"A half-savage shepherd",
+	"A charismatic priest",
+	"A major landowner",
+	"A youth who wants out",
+	"Venerable old farmer",
+	"Criminal in rural hiding",
+	"Native hedge mage",
+	"A noble's local reeve",
+	"Important artisan"
+];
+exports.aCurrentPressingProblem = [
+	"Vital food stores have been lost or stolen",
+	"Two families are in a long-running feud",
+	"A new religion is making trouble with the old",
+	"The lord's reeve has been given unreasonable quotas I",
+	"An upstart newcomer is disrupting the native hierarchy",
+	"A socially marginal family is suspected of crimes",
+	"A dangerous beast or foe is lurking near the place",
+	"A local bully and his friends are causing trouble",
+	"The crops or herds are in very poor condition",
+	"An ambiguous land dispute is getting bloody",
+	"A curse or magical woe is vexing the people",
+	"Noble quarrels have caused collateral damage"
+];
+exports.localLikelyToInteractWithAdventurers = [
+	"An ambitious local who wants to get to the city",
+	"Social outcast who can't ask any local for help",
+	"Gentry who wants no local gossip about their need",
+	"Ex-city dweller eager for contact with the worldly",
+	"Comparatively rich villager with a need",
+	"Starry-eyed young local swooning for adventurers",
+	"Village priest in need of outside help",
+	"Reeve who can't reasonably call on natives for a job",
+	"Village leader who wants expendable help",
+	"Burly local militiaman who watches new faces here",
+	"Shifty native who wants some clueless accomplices",
+	"Clan patriarch who doesn't want to use their own kin"
+];
+exports.interestingThingsThePlaceCanOfferHeroes = [
+	"An unusually large amount of saved coinage",
+	"Valuable local specialty product",
+	"Heirloom magic item passed down to the leader",
+	"A magical service unique to a local tradition",
+	"A map or secret leading to a Deep or plunder site",
+	"A temple with potent healing magics available",
+	"A secret treasure acquired generations ago",
+	"Large amounts of produce or local products",
+	"Ownership of an abandoned farm or structure",
+	"Contacts with rural bandits or criminal rings",
+	"Favors from the lord who's very fond of the village",
+	"Young locals willing to take service with the party"
+];
---
+++ src/backend/table roller/tables/Location Tags/index.js
@@ -0,0 +1,1 @@
+exports.locationTags = require("./WWN/tags");
