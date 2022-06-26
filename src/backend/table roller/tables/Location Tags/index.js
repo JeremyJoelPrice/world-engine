2a2c71e7102b18618c783cae361c3c8cde0f1f53
Jeremy Joel Price <JeremyJoelPrice@gmail.com>
2022-06-26 12:40:52 +0100

    add community types to tables index

--- src/backend/table roller/tables/Location Tags/index.js
+++ src/backend/table roller/tables/Location Tags/index.js
@@ -1,1 +1,4 @@
 exports.locationTags = require("./WWN/tags");
+exports.ruralVillages = require("./WWN/ruralVillages");
+exports.majorCities = require("./WWN/majorCities");
+exports.tribesAndExiles = require("./WWN/tribesAndExiles");
