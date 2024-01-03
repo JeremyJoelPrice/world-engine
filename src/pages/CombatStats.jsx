b29784a31fba0060e40b213244821de7d571a546
Jeremy Joel Price <JeremyJoelPrice@gmail.com>
2024-01-03 19:59:19 +0000

    implement editable stat row

--- src/pages/CombatStats.jsx
+++ src/pages/CombatStats.jsx
@@ -6,13 +6,22 @@ import {
 	getStats,
 	roleOptions
 } from "../services/combatStatsService";
+import genericMonsterStats from "../datasource/genericMonsterStats";
 import { useState } from "react";
 
 const CombatStats = () => {
+	const [rows, setRows] = useState([]);
+
+	// const copyRow = (cr, role) => {};
+
 	return (
 		<>
 			<Header>Combat Stats</Header>
-			<StatRow />
+			<CopyButton>Copy Row</CopyButton>
+			<StatRow initialCR={crOptions[0]} initialRole={roleOptions[0]} />
+			{rows.map((r) => {
+				return <StatRow key={r} />;
+			})}
 		</>
 	);
 };
@@ -21,25 +30,37 @@ export default CombatStats;
 
 /* Other Components */
 
-const StatRow = () => {
-	const [role, setRole] = useState(roleOptions[0]);
-	const [cr, setCR] = useState(crOptions[0]);
+const StatRow = ({ initialCR, initialRole }) => {
+	const [role, setRole] = useState(initialRole);
+	const [cr, setCR] = useState(initialCR);
 	const [stats, setStats] = useState(getStats(cr, role));
+
+	// console.log("[CombatStats]");
+	// console.log("	cr: ", cr);
+	// console.log("	role: ", role);
+	// console.log("	stats: ", stats);
 
 	const handleChange = (event) => {
 		event.preventDefault();
 		const { name, value } = event.target;
 		switch (name) {
 			case "role":
-				setRole(value);
+				setRole((prevRole) => {
+					const newRole = value;
+					setStats(getStats(cr, newRole));
+					return newRole;
+				});
 				break;
 			case "cr":
-				setCR(value);
+				setCR((prevCr) => {
+					const newCr = Number(value);
+					setStats(getStats(newCr, role));
+					return newCr;
+				});
 				break;
 			default:
 				break;
 		}
-		setStats(getStats(cr, role));
 	};
 
 	return (
@@ -71,6 +92,10 @@ const StatRow = () => {
 	);
 };
 
+const NewRowButton = () => {
+	return <StyledAddButton>+</StyledAddButton>;
+};
+
 /* Styled Components */
 
 const StyledStatRow = styled.div`
@@ -87,6 +112,37 @@ const StyledDropdown = styled.select`
 	outline: none;
 	font-family: Times, "Times New Roman", Georgia, serif;
 	font-size: 14pt;
-
+	min-width: 65px;
+	text-align: center;
 	margin: 0 10px 0 0;
+`;
+
+const StyledAddButton = styled.button`
+	background-color: transparent;
+	color: ${colors.cream2};
+	border: none;
+	box-shadow: beige 0px 0px 1px 1px;
+	border-radius: 25px;
+	margin: 1px 10px 0;
+	height: 1.5em;
+
+	&:hover {
+		cursor: pointer;
+	}
+`;
+
+const CopyButton = styled.button`
+	display: block;
+
+	margin: 0 auto 10px;
+	padding: 5px 10px;
+
+	font-family: Georgia, "Times New Roman", Times, serif;
+	font-size: 16pt;
+	color: ${colors.cream};
+	background-color: ${colors.darkgrey};
+	border-radius: 6px;
+	&:hover {
+		cursor: pointer;
+	}
 `;
--- src/services/combatStatsService.js
+++ src/services/combatStatsService.js
@@ -5,8 +5,8 @@ import genericMonsterStats from "../datasource/genericMonsterStats";
  */
 
 const crOptions = [
-	0.125, 0.25, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
-	18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30
+	0, 0.125, 0.25, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+	17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30
 ];
 
 const roleOptions = [
@@ -25,8 +25,13 @@ const roleOptions = [
 
 const getStats = (cr, role) => {
 	const stats = genericMonsterStats.filter(
-		(statblock) => statblock.cr == cr
+		(statblock) => statblock.cr === cr
 	)[0];
+
+	// console.log("[getStats()]");
+	// console.log("	cr: ", cr);
+	// console.log("	role: ", role);
+	// console.log("	stats: ", stats);
 
 	return stats;
 };
